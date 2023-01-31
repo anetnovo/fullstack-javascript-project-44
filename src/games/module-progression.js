8d3c6a74b4c6e2c0c5fdec63019b9289b10442b0
anetnovo <polozhentseva.alena@mail.ru>
2023-01-31 20:24:10 +0300

    add module-progression

--- src/games/module-progression.js
+++ src/games/module-progression.js
@@ -6,7 +6,7 @@ import {
 export default function game() {
   const name = getName();
   printTask('What number is missing in the progression?');
-  const getIndex = () => {
+  const getRandomIndex = () => {
     const arr = [];
     const rand = Math.floor(Math.random() * 10);
     for (let i = 0; i < 10; i += 1) {
@@ -16,7 +16,7 @@ export default function game() {
   };
   const makeProgression = () => {
     const start = randomNum();
-    const hide = getIndex();
+    const hide = getRandomIndex();
     const step = randomNum();
     const prog = [];
     for (let i = start; i <= 10 * step + start; i += step) {
@@ -30,12 +30,12 @@ export default function game() {
   for (let attempt = 1; attempt <= 3; attempt += 1) {
     const { finishProgression, result } = makeProgression();
     printQuestion(finishProgression);
-    const result2 = result;
+    const correctAnswer = result;
     const answer = readlineSync.question('Your answer: ');
-    if (Number(answer) === result2) console.log('Correct!');
-    if (attempt === 3 && Number(answer) === result2) congrats(name);
-    else if (Number(answer) !== result2) {
-      wrongAnswer(answer, result2, name);
+    if (Number(answer) === correctAnswer) console.log('Correct!');
+    if (attempt === 3 && Number(answer) === correctAnswer) congrats(name);
+    else if (Number(answer) !== correctAnswer) {
+      wrongAnswer(answer, correctAnswer, name);
       break;
     }
   }
