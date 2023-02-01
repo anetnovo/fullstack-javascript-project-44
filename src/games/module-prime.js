97bb3ca684b4b00ea8a173b33f98f0fd0b6c33ad
anetnovo <polozhentseva.alena@mail.ru>
2023-02-01 20:29:53 +0300

    take out function primeCheck

--- src/games/module-prime.js
+++ src/games/module-prime.js
@@ -3,20 +3,20 @@ import {
   getName, randomNum, printTask, printQuestion, congrats, wrongAnswer,
 } from '../index.js';
 
+const primeCheck = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 export default function game() {
   const name = getName();
   printTask('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const primeCheck = (num) => {
-    if (num < 2) {
-      return false;
-    }
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
   const sayPrimeOrNot = (num) => {
     const text = primeCheck(num) ? 'yes' : 'no';
     return text;
