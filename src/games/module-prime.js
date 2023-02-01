60c44a1d7dad83376abbc98950d9427f8fdec9ee
anetnovo <polozhentseva.alena@mail.ru>
2023-02-01 20:07:19 +0300

    change if condition in module-prime

--- src/games/module-prime.js
+++ src/games/module-prime.js
@@ -26,8 +26,11 @@ export default function game() {
     printQuestion(number);
     const result = sayPrimeOrNot(number);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === result) console.log('Correct!');
-    if (attempt === 3 && answer === result) congrats(name);
+    if (answer === result) {
+      console.log('Correct!');
+    if (attempt === 3) congrats(name);
+    }
+   // if (attempt === 3 && answer === result) congrats(name);
     else if (answer !== result) {
       wrongAnswer(answer, result, name);
       break;
